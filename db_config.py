b8e99413a9c477cbdd24f1c54e2505e66fa6f1a7
kvtodev <kvtodev@outlook.com>
2025-03-17 19:54:30 +0800

    use server db config

--- db_config.py
+++ db_config.py
@@ -1,7 +1,7 @@
 # 数据库连接配置
 DB_CONFIG = {
-    "user": "root",
-    "password": "root",
+    "user": "sqluser",
+    "password": "bieS+AjfocW1bzqsPy08DWCIOAV1Av6jo030U+ZVlQY=",
     "port": 3306,
     "host": "127.0.0.1",
     "db": "school",
